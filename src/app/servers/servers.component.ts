77ce5dd2457ddd15b3153d5d8a4721c4b161dd80
Daniels <daniel.sadowski75@gmail.com>
2019-04-09 23:10:09 +0200

    init commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit,Form } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated:boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event:Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+    this.serverCreated = true;
+  }
+}
